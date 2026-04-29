2b422afada5a5ecdeef8f859faa9c0e23cd042d0
Daniel Gil <dani.gil.mayol@gmail.com>
2026-04-29 13:34:38 +0200

    optimize the quickSort algorithm to use recursion

--- SortingAlgorithms/QuickSort.cs
+++ SortingAlgorithms/QuickSort.cs
@@ -8,122 +8,48 @@ public class QuickSort : ISorter
         {
             return;
         }
-        
-        var visited = new bool[array.Length];
-        
-        var isSorted = false;
-        while (!isSorted)
-        {
-            // update the pivot and indexes
-            var (startIndex, endIndex) = GetIndexes(array, visited);
-            
-            var pivotIndex = PivotIteration(array, startIndex, endIndex);
-            
-            // we mark the pivot index (endIndex) as visited
-            visited[pivotIndex] = true;
 
-            if (visited.All(x => x))
-            {
-                isSorted = true;
-            }
+        SortRange(array, 0, array.Length - 1);
+    }
+    
+    private static void SortRange(int[] array, int start, int end)
+    {
+        if (start >= end)
+        {
+            // stop the recursion when:
+            //    - (start==end) -> single item array
+            //    - (start>end)  -> empty array
+            return;
         }
+        
+        // calculate partitions
+        var boundary = Partition(array, start, end);
+
+        // sort left
+        SortRange(array, start, boundary - 1);
+
+        // sort right
+        SortRange(array, boundary + 1, end);
     }
 
-    private static (int startIndex, int endIndex) GetIndexes(int[] array, bool[] visited)
+    // this returns the index of the pivot after it has been moved to its right position (sorted)
+    // we assume that the pivot is the last element of the array 
+    private static int Partition(int[] array, int start, int end)
     {
-        int? startIndex = null, endIndex = null;
+        var pivot = array[end];
         
-        for (var i = 0; i < array.Length; i++)
+        // The boundary marks the boundary of the left and right partitions (represents the end of the left partition).
+        // On the first iteration it will be -1 meaning the left partition is empty
+        var boundary = start - 1 ; 
+
+        for (var i = start; i <= end; i++)
         {
-            // initially we search for the first non-visited element
-            if (startIndex is null && visited[i])
+            if(array[i] <= pivot)
             {
-                continue;
-            }
-            
-            if (startIndex is null && !visited[i])
-            {
-                // here we found the first non-visited element, so we initialize both indexes to this index.
-                // From now on we need just to take care of the endIndex
-                startIndex = i;
-                endIndex = i;
-                continue;
-            }
-            
-            if(startIndex is not null &&
-               endIndex is not null &&
-               visited[i])
-            {
-                // here we had a range initialized and found a stop condition (a visited node), we end our search here
-                return (startIndex.Value, endIndex.Value);
-            }
-            
-            if (endIndex is not null)
-            {
-                // here we had a range initialized and still in non-visited nodes, so we keep updating the endIndex
-                endIndex = i;    
+                array.Swap(i, ++boundary);
             }
         }
-
-        return (startIndex!.Value, endIndex!.Value);
-    }
-
-    private static int PivotIteration(int[] array, int startIndex, int endIndex)
-    {
-        var pivotValue = array[endIndex]; // select the last item as the pivot
-        var (leftPartition, rightPartition) = BuildPartitions(array, pivotValue, startIndex, endIndex);
-        var pivotIndex = UpdateArray(pivotValue, startIndex, leftPartition, rightPartition, array);
-        return pivotIndex;
-    }
-
-    private static (List<int> leftPartition, List<int> rightPartition) BuildPartitions(
-        int[] array,
-        int pivotValue, 
-        int startIndex, 
-        int endIndex)
-    {
-        List<int> leftPartition = [];
-        List<int> rightPartition = [];
         
-        // notice that we are not processing the pivot index (endIndex)
-        for (var i = startIndex; i < endIndex; i++)
-        {
-            if (array[i] < pivotValue)
-            {
-                leftPartition.Add(array[i]);
-            }
-            else
-            {
-                rightPartition.Add(array[i]);
-            }
-        }
-
-        return (leftPartition, rightPartition);
-    }
-
-    private static int UpdateArray(
-        int pivotValue,
-        int startIndex, 
-        List<int> leftPartition, 
-        List<int> rightPartition, 
-        int[] array)
-    {
-        // first process the left partition
-        if (leftPartition.Count > 0)
-        {
-            Array.Copy(leftPartition.ToArray(), 0, array, startIndex, leftPartition.Count);
-        }
-
-        // second process the pivot
-        var pivotIndex = startIndex + leftPartition.Count;
-        array[pivotIndex] = pivotValue;
-        
-        // finally process the right partition
-        if (rightPartition.Count > 0)
-        {
-            Array.Copy(rightPartition.ToArray(), 0, array, pivotIndex+1, rightPartition.Count);    
-        }
-        
-        return pivotIndex;
+        return boundary;
     }
 }
